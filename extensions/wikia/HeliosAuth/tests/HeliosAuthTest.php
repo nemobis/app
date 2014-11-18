22ebe4cc5c816e99e838447b173e9d4f815f8039
Michał Roszka <michal@roszka.pl>
2014-11-19 00:37:40 +0100

    add template setUp() and tearDown() methods in HeliosAuthTest

--- extensions/wikia/HeliosAuth/tests/HeliosAuthTest.php
+++ extensions/wikia/HeliosAuth/tests/HeliosAuthTest.php
@@ -1,3 +1,12 @@
 <?php
 class HeliosAuthTest extends WikiaBaseTest {
+    
+    public function setUp() {
+        parent::setUp();
+    }
+    
+    public function tearDown() {
+        parent::tearDown();
+    }
+    
 }
